2ee2ad550cc46bc7b2996738f37d5757ab652899
Alainsol <85121697+Alainsol@users.noreply.github.com>
2021-07-27 23:42:42 +0700

    update update

--- lesson-9/prophets.js
+++ lesson-9/prophets.js
@@ -15,7 +15,21 @@ fetch(requestURL)
         for (let i = 0; i < prophets.length; i++ ) {
 
             let card = document.createElement('section');
-            let title = document.createElement('h2');
+            let h2 = document.createElement('h2');
+            let p = document.createElement('p');
+            let img = document.createElement('img');
+            h2.textContent = prophets[i].name + ' ' + prophets[i].lastname + ' - ' + prophets[i].order;
+            p.innerHTML = 'Birthdate: ' + prophets[i].birthdate + '<br>' +
+            'Years Served: ' + prophets[i].length + '<br>' +
+            'Birthplace: ' + prophets[i].birthplace + '</p>';
+            img.setAttribute('src', prophets[i].imageurl);           
+            card.appendChild(h2).appendChild(p).appendChild(img);
+            document.querySelector('div.cards').appendChild(card);
+        }
+    });
+/*
+ let card = document.createElement('section');
+            let h2 = document.createElement('h2');
             title.textContent = prophets[i].name + ' ' + prophets[i].lastname;
 
             let birthdate = document.createElement("p");
@@ -45,6 +59,6 @@ fetch(requestURL)
             document.querySelector('div.cards').appendChild(card);
         }
 
-      });
+      });*/
 
     
