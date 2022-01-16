c9be2c59a1bff5247cf08670cda7be9919a49cbe
sol21002@byui.edu <85121697+Alainsol@users.noreply.github.com>
2022-01-16 22:54:48 +0700

    updated cssu

---
+++ css/js/lastModified.js
@@ -0,0 +1,10 @@
+function lastModified() {
+    document.write(document.lastModified);
+}
+
+lastModified();
+/*t conslastModified = document.lastModified;
+document.querySelector("#Date_Modified").textContent = lastModified;
+
+const year = new Date().getFullYear();
+ document.querySelector("#Current_Year").textContent = year;*/
