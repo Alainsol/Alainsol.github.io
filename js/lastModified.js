e17690d688a8e47644703793eb165ead19ced604
sol21002@byui.edu <85121697+Alainsol@users.noreply.github.com>
2022-01-22 15:02:23 +0700

    unpdated lesson3

--- js/lastModified.js
+++ js/lastModified.js
@@ -3,8 +3,8 @@ function lastModified() {
 }
 
 lastModified();
-/*t conslastModified = document.lastModified;
+ conslastModified = document.lastModified;
 document.querySelector("#Date_Modified").textContent = lastModified;
 
 const year = new Date().getFullYear();
- document.querySelector("#Current_Year").textContent = year;*/
+ document.querySelector("#Current_Year").textContent = year;
