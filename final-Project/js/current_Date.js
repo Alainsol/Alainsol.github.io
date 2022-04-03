bcb697e6e0e3f24b3aa046cabf0b43d15f065c3c
sol21002@byui.edu <85121697+Alainsol@users.noreply.github.com>
2022-04-03 09:14:49 +0700

    update footer

---
+++ final-Project/js/current_Date.js
@@ -0,0 +1,5 @@
+const lastModified = document.lastModified;
+document.querySelector("#Date_Modified").textContent = lastModified;
+
+const year2 = new Date().getFullYear();
+ //document.querySelector("#Current_Year").textContent = year2;
